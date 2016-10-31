960432e097a8ee3816d13ace826433d1c2f4e743
Joy Liu <liukang.joy@gmail.com>
2016-10-31 08:32:38 -0400

    automated app tests

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,9 @@
+const expect = require('expect');
+
+// use `it` function to specify the function name
+
+describe('App', () => {
+    it('should properly run tests', () => {
+        expect(1).toBe(1);
+    })
+})
